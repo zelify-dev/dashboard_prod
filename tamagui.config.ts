50f2a613f958feba523fafdc941350c57c112646
Vicente0709 <vicentenarvaez.mln@icloud.com>
2026-07-31 14:09:25 -0500

    style: sobreescribir la configuración de fuentes de Tamagui para usar exclusivamente Nata Sans

--- tamagui.config.ts
+++ tamagui.config.ts
@@ -1,6 +1,16 @@
 import { config } from '@tamagui/config/v3'
 import { createTamagui } from 'tamagui'
 
+// Sobreescribir las familias de fuentes por defecto de Tamagui para usar exclusivamente Nata Sans
+const fontsObj = config.fonts as any
+if (fontsObj) {
+  Object.keys(fontsObj).forEach((key) => {
+    if (fontsObj[key]) {
+      fontsObj[key].family = 'Nata Sans, sans-serif'
+    }
+  })
+}
+
 const tamaguiConfig = createTamagui(config)
 
 export type AppConfig = typeof tamaguiConfig
